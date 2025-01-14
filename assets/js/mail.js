b0af58dfe8e9d4a9d7633bc5adf1c77b9ed4dec0
Mikita Besau <mikitabesau@gmail.com>
2025-01-14 18:57:08 +0200

    first commit

---
+++ assets/js/mail.js
@@ -0,0 +1,9 @@
+function sendEmail() {
+	const name = encodeURIComponent(document.getElementById("name").value);
+	const email = encodeURIComponent(document.getElementById("email").value);
+	const message = encodeURIComponent(document.getElementById("message").value);
+
+	const mailtoLink = `mailto:mikitabesau@gmail.com?subject=Message%20from%20${name}&body=Name:%20${name}%0AEmail:%20${email}%0A%0AMessage:%0A${message}`;
+
+	window.location.href = mailtoLink;
+}

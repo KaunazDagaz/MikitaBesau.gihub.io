9ff892023e1e6ab4a3bd020f497923d7980705b1
Mikita Besau <mikitabesau@gmail.com>
2025-01-15 12:23:49 +0200

    style: :lipstick: added back buttons, changed message style

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -3,7 +3,7 @@ function sendEmail() {
 	const email = encodeURIComponent(document.getElementById("email").value);
 	const message = encodeURIComponent(document.getElementById("message").value);
 
-	const mailtoLink = `mailto:mikitabesau@gmail.com?subject=Message%20from%20${name}&body=Name:%20${name}%0AEmail:%20${email}%0A%0AMessage:%0A${message}`;
+	const mailtoLink = `mailto:mikitabesau@gmail.com?subject=Message%20from%20${name}&body=Email for contact:%20${email}%0A%0AMessage:%0A${message}`;
 
 	window.location.href = mailtoLink;
 }
